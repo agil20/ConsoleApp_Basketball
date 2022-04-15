876a0e568e29a1e65657e01c61be563cb662e25e
agil20 <agilakh@code.edu.az>
2022-04-15 14:19:35 +0400

    feat:I opened the interface, added groups and player class

---
+++ ConsoleApp_Basketball/DataAcces/DataConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entites.Models;
+
+namespace DataAcces
+{
+    public class DataConnect
+    {
+        public static List<Player>Players { get; set; }
+        public static List<Groups>Groups  { get; set; }
+
+    }
+}
